61e89fc2e7373ca201f78d255a3681ab1abb91e9
Sumit Murari <vaishali.bariwal@gmail.com>
2020-02-09 23:03:25 +0530

    background image

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'thirdScreen.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -25,86 +26,96 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-  return Scaffold(
-      appBar: AppBar(
-      title: Text(widget.title),
-      ),
-      body: Container(
-      margin: EdgeInsets.fromLTRB(80, 40, 0, 30),
-        child:new Column(
-         // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-          
-          new Text("        "),new Text("        "),new Text("        "),new Text("        "),
-          ButtonTheme(
-          minWidth: 250.0,
-          height: 60.0,
-           buttonColor: Colors.red,
-          child: RaisedButton(
-          
-          child: Text('MARK YOUR LOACTION',
-            style: TextStyle(fontSize: 18,
-            fontWeight: FontWeight.bold, color: Colors.white),
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Background Image Example'),
+        ),
+        body: Stack(children: <Widget>[
+          Center(
+            child: new Image.asset(
+              'assets/images/home.jpg',
+              width: size.width,
+              height: size.height,
+              fit: BoxFit.fill,
             ),
-          shape: RoundedRectangleBorder(
-            borderRadius: new BorderRadius.circular(38.0),
-            side: BorderSide(color: Colors.red,width: 2.0)
           ),
-          onPressed: () {
-            Navigator.of(context)
-                .push(MaterialPageRoute<Null>(builder: (BuildContext context) {
-              return new SecondScreen();
-          }));      
-        }
-        )
-      ),
-      new Text("        "),new Text("        "),new Text("        "),
-      ButtonTheme(
-          minWidth: 250.0,
-          height: 60.0,
-           buttonColor: Colors.red,
-          child: RaisedButton(
-          
-          child: Text('MARK YOUR LOACTION',
-            style: TextStyle(fontSize: 18,
-            fontWeight: FontWeight.bold, color: Colors.white),
+          Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                ButtonTheme(
+                    minWidth: 250.0,
+                    height: 60.0,
+                    buttonColor: Colors.red,
+                    child: RaisedButton(
+                        child: Text(
+                          'MARK YOUR LOACTION',
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(38.0),
+                            side: BorderSide(color: Colors.red, width: 2.0)),
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute<Null>(
+                              builder: (BuildContext context) {
+                            return new ThirdScreen();
+                          }));
+                        })),
+                new Text("        "),
+                new Text("        "),
+                new Text("        "),
+                new Text("        "),
+                ButtonTheme(
+                    minWidth: 250.0,
+                    height: 60.0,
+                    buttonColor: Colors.red,
+                    child: RaisedButton(
+                        child: Text(
+                          'SAVED LOACTION',
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(38.0),
+                            side: BorderSide(color: Colors.red, width: 2.0)),
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute<Null>(
+                              builder: (BuildContext context) {
+                            return new SecondScreen();
+                          }));
+                        })),
+              ],
             ),
-          shape: RoundedRectangleBorder(
-            borderRadius: new BorderRadius.circular(38.0),
-            side: BorderSide(color: Colors.red,width: 2.0)
-          ),
-          onPressed: () {
-            Navigator.of(context)
-                .push(MaterialPageRoute<Null>(builder: (BuildContext context) {
-              return new ThirdScreen();
-          }));      
-        }
-        )
-      )
-          ]
-      )
-      )
-  );
+          )
+        ]));
   }
 }
+
 class SecondScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar( 
+      appBar: AppBar(
         title: Text('Second Screen'),
       ),
       body: Center(
         child: RaisedButton(
           color: Colors.red,
-          child: Text('MARK YOUR LOACTION',
-            style: TextStyle(fontSize: 18,
-            fontWeight: FontWeight.bold, color: Colors.white),
-            ),
-          shape: RoundedRectangleBorder(
-            borderRadius: new BorderRadius.circular(38.0),
-            side: BorderSide(color: Colors.red,width: 2.0)
+          child: Text(
+            'MARK YOUR LOACTION',
+            style: TextStyle(
+                fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
           ),
+          shape: RoundedRectangleBorder(
+              borderRadius: new BorderRadius.circular(38.0),
+              side: BorderSide(color: Colors.red, width: 2.0)),
           onPressed: () {
             //Use`Navigator` widget to pop oir go back to previous route / screen
             Navigator.pop(context);
